84ecfe3d577f3c39c9b669079a0810b147971064
Richard Winterhalder <if12b088@technikum-wien.at>
2015-09-27 12:55:05 +0200

    progam  finished

--- ASWT.CoverageLib/Coverage.cs
+++ ASWT.CoverageLib/Coverage.cs
@@ -8,10 +8,19 @@ namespace ASWT.CoverageLib
 {
     public static class Coverage
     {
-        public static int run()
+        public static int Run(int a, int b)
         {
-
-            return 0;
+            if (a >= b){
+                a += 20;
+                if (a == 70 && b == 50) return -1;
+                if (a > 100) return 100;
+                return a;
+            }
+            else {
+                b += 10;
+                if (b > 100) return 100;
+                return b;
+            }
         }
     }
 }
---
+++ ASWT.CoverageUintTest/CoverageUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ASWT.CoverageLib;
+
+namespace ASWT.CoverageUintTest
+{
+    [TestClass]
+    public class CoverageUnitTest
+    {
+        [TestMethod]
+        public void TC1_1()
+        {
+            var sut = Coverage.Run(10, 5);
+            Assert.AreEqual(30, sut);
+        }
+
+        [TestMethod]
+        public void TC1_2()
+        {
+            var sut = Coverage.Run(120, 5);
+            Assert.AreEqual(100, sut);
+        }
+
+        [TestMethod]
+        public void TC1_3()
+        {
+            var sut = Coverage.Run(10, 50);
+            Assert.AreEqual(60, sut);
+        }
+
+        [TestMethod]
+        public void TC1_4()
+        {
+            var sut = Coverage.Run(10, 99);
+            Assert.AreEqual(100, sut);
+        }
+
+        [TestMethod]
+        public void TC1_5()
+        {
+            var sut = Coverage.Run(10, 89);
+            Assert.AreEqual(99, sut);
+        }
+    }
+}
