a6d813769bd268fe788f86baa8c32b49c965598c
Richard Winterhalder <if12b088@technikum-wien.at>
2015-09-27 11:15:42 +0200

    init commit

---
+++ ASWT.CoverageLib/Coverage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASWT.CoverageLib
+{
+    public static class Coverage
+    {
+        public static int run()
+        {
+
+            return 0;
+        }
+    }
+}
